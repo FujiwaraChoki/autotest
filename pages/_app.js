df4bbec451f7c7443d63aec6adacdb956418df68
Sami Hindi <lolipop66611@gmail.com>
2023-06-14 13:20:29 +0200

    Added Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ const App = ({ Component, pageProps }) => {
       <UserProvider value={{ user, setUser }}>
         <Head>
           <title>AutoTest</title>
+          <link rel="icon" href="/logo.png" />
           <meta name="description" content="AutoTest ist eine Plattform, welche dir die Macht gibt, personalisierte Tests zu erstellen." />
         </Head>
         <Navbar />
