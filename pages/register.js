5a724a89b7008005412fe35aaf6a8d2bd7f85e76
Sami Hindi <lolipop66611@gmail.com>
2023-06-21 13:25:03 +0200

    Added method to fetch

--- pages/register.js
+++ pages/register.js
@@ -8,6 +8,7 @@ const register = () => {
 
     const register = async () => {
         const response = await fetch("/api/register", {
+            method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
